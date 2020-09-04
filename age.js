ab37d5fe175f0f2e4508885969587ddc420c54e0
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-05 08:40:30 +0900

    Update age.js

--- age.js
+++ age.js
@@ -1,9 +1,8 @@
-'use strict';
-{
+'use strict'; {
 // 年齢選択をドロップダウン表示
   var age = document.getElementById("age");
   for (var i = 15; i<= 50; i++) {
-    var option = document.createElement("option"); //optionタグ作成
+    var option = document.createElement("option"); //optionタグ
     // option.value = i;
     option.innerText = i +'歳'; //テキスト
     age.appendChild(option);
