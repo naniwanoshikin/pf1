b6a53f9ced473f7a58da9ad33dc4c2ded927d6ed
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-05 08:39:27 +0900

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,26 +1,25 @@
 'use strict';
-{ // モーダル機能を実装
+{ // モーダル機能
 
-  // 各idを定義、取得する
+  // idを取得、定義
   const open = document.getElementById('open');
   const close = document.getElementById('close');
   const mask = document.getElementById('mask');
   const modal = document.getElementById('modal');
 
-  // hiddenクラス除去（モーダル表示）
+  // モーダル表示（hiddenクラス除去）
   open.addEventListener('click', () => {
       mask.classList.remove('hidden');
       modal.classList.remove('hidden');
     });
     
-  // hiddenクラス付与（元の画面へ）
-  close.addEventListener('click', () => {
-      mask.classList.add('hidden');
-      modal.classList.add('hidden');
-    });
+  // 元の画面へ（hiddenクラス付与）
+  // close.addEventListener('click', () => {
+  // });
   mask.addEventListener('click', () => {
-    // 上記同様の文で以下と書ける。
-      close.click();
+    mask.classList.add('hidden');
+    modal.classList.add('hidden');
+      // close.click(); // 上記同様の文。
     });
   
 
