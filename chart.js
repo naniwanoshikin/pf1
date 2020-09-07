5dde3f304660423eba4037d4d29b169afabccc14
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-07 15:40:14 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -5,7 +5,7 @@
   var data = {
     labels: ['HTML CSS', 'Sass', 'JScript', 'jQuery', 'React', 'PHP'],
     datasets: [{
-      label: '@me',
+      label: '@ 2020.9',
       data: [40, 30, 30, 30, 15, 5],
       backgroundColor: 'rgba(255, 0, 0, 0.1)',
       borderColor: 'red',
@@ -15,8 +15,8 @@
       pointStyle: 'rect',
     }, 
     {
-      label: '@ito',
-      data: [90, 85, 80, 80, 50, 10],
+      label: '@2021.9(est.)',
+      data: [90, 85, 90, 80, 70, 40],
       backgroundColor: 'rgba(0, 0, 255, 0.1)',
       borderColor: 'blue',
       borderWidth: 1,
@@ -32,30 +32,32 @@
         fontColor: 'purple',
       },
       // yAxes: [{
-        ticks: {
-          beginAtZero: true, // 自動最小
-          max: 100, // 最大
-          // suggestedmax: 100, // 自動最大
-          stepSize: 20, // 刻み
-          // fontSize: 30,
-          callback: function(value, index, values) {
-            return value + '-';
-          }
+      ticks: {
+        beginAtZero: true, // 自動最小
+        max: 100, // 最大
+        // suggestedmax: 100, // 自動最大
+        stepSize: 20, // 刻み
+        // fontSize: 30,
+        callback: function(value, index, values) {
+          return value + '-';
         }
+      }
       // }]
     }, 
     title: {
       display: false,
       text: '言語',
       fontSize: 18,
-      // position: 'left'
     },
     animation: {
       // duration: 0
     },
     legend: { // 判例
-      position: 'bottom', // 右側      
       // display: false // なくす
+      position: 'bottom',
+      labels: {
+        fontSize: 16,
+      }
     },
     // responsive: true, // グラフサイズ用（理解できん、、
     // maintainAspectRatio: false, // （理解できん、、
