c513c68dd70b9067d56708950a2bf5ce9b9bc7b9
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-12 06:53:33 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -1,65 +1,26 @@
 $(function() {
 
-  // jQuery
 
-  // $('p').css('color', 'red').css('background', 'skyblue').hide(7500); // メソッドチェーン
-  // $('p').addClass('style'); //クラスつける
-  // $('p').text('change'); // 文字列を書き換える
-  // $('p').html('<a href="#about" data-all="pro">自己紹介</a>'); //タグも含めて取得
-  // $('p').empty(); // 文字を消す
-  // $('p').remove(); // 要素自体を消す
-  // $('input').val('helloagain'); // 値を入力
-  // $('aside').css('display', 'block'); //  
-  // 属性セレクタ
-  // $('a[href="members/index.html"]').css('background', 'red');
-  // $('a[href!="members/index.html"]').css('background', 'red'); //~ない
-  // $('a[href*="index"]').css('background', 'red'); //含む
-  // $('a[href$="t"]').css('background', 'red'); //で終わる
-  // console.log($('a').attr('href')); // 属性の値を取得
-  // console.log($('a').data('all')); // カスタムデータ属性取得
-  // $('#open').toggle(1500); // 繰り返し処理
-  // $('#open').fadeOut(1500, function() { // 消えた後、
-    // alert('okkkkkkkkk!');  // アラート表示
-  // });
-  // $('aside').slideDown(1500); // 元々非表示にしておく
-
-  // english.js
-
-// function update() { // 自動切り替え（クリックなし）
-//   id属性がついていないセレクタ→querySelector
-//   最初の1つを取得
-//   document.querySelector('h2').textContent = '式っち!';
-//   指定したh2要素
-//   document.querySelectorAll('h2')[1].textContent = '式っち!';
-// }
-// setTimeout(update, 1000);
-
-
-//   gitの状態
-// １、作業ディレクトリ（ファイルの作成、編集）
-// ２、ステージングエリア（インデックス）を経由して、
-// ３、リポジトリに保存（ローカル or リモート）
-
-
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // header
 
 // h1大きさ
 $('#install')
-.mouseover(function() {  //マウスがのった時
+.mouseover(function() {  // マウスがのった時
   $(this).animate({'font-size': '20px'}, 300);
 })
-.mouseout(function() {  //マウスが外れた時
+.mouseout(function() {  // マウスが外れた時
   $(this).animate({'font-size': '18px'}, 300);
 })
 .mousemove(function(e) {
-  // $(this).text(e.pageX);  //マウスの座標テキストイベントを表示
+  // $(this).text(e.pageX);  // マウスの座標テキストイベントを表示
 });
 
 // h1へ戻る
 $('#install').click(function() {
   $('html, body').animate(
     {'scrollTop': 0},
-    350); //速さ
+    350); // 速さ
 });
 
 // ナビスクロール
@@ -70,48 +31,50 @@ $('header a').click(function() {
     'scrollTop': position - 100 // その高さへ
   }, 'fast');
 });
-// ドロップダウンメニュー
+// ドロップダウンメニュー（アコーディオン機能）
 $('.menubtn').click(function() { // クリックしたとき、
   var $answer = $('nav').find('ul'); // nav直下のulが
   if($answer.hasClass('open')) { // openしていたら、
     $answer.removeClass('open'); // openやめて
-    $answer.slideUp(); // 隠す
+    $answer.slideUp(); // 隠す。
     // $('nav').find('.menubtn').text('++'); // 書き換え
   } else { // openしていなかったら、
-    $answer.addClass('open'); // openする 
-    $answer.slideDown(); // 表示
+    $answer.addClass('open'); // openして
+    $answer.slideDown(); // 表示。
     // $('nav').find('.menubtn').text('--'); // 書き換え    
   }
 });
 
-
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // main
 
-// Vanish!!!ボタン
+// [Vanish]ボタン
 $('.van').click(function() {
-  var p = $('<p id="vani" style="font-size: 24px;">').text('Hello World!').addClass('vanish'); //vanish
-  $('#wellcome').after(p); //前に挿入
+  var p = $('<h2 id="vani" style="font-size: 24px;">').text('Hello World!').addClass('vanish'); // vanish
+  $('#root1').after(p); // 前に挿入
 });
 // ONメソッド：読み込まれた時に存在していない要素を処理
 $('body').on('click', '.vanish', function() { //clickするとvanishする機能
-  $(this).remove(); //削除
+  $(this).remove(); // 削除
 });
 
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // 取り消し線
 $('.script > li').click(
   function() {
   $(this).css('text-decoration', 'line-through');
 });
 
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // 地域紹介
 //◀️▶️ボタン表示の状態について定義（下記1, 2に適用）
 function toggleChange() {
-  var slideIndex = $('.slide').index($('.active')); //アクティブ番号
-  $('.change').show(); //◀️ボタン表示
-  if (slideIndex === 0) {  //最初の時
-    $('.prev').hide(); //◀️ボタン消す
+  var slideIndex = $('.slide').index($('.active')); //アクティブ状態になっている番号を取得
+  $('.change').show(); // ◀️ボタン表示
+  if (slideIndex === 0) {  // 最初の時
+    $('.prev').hide(); // ◀️ボタン消す
   } else if (slideIndex === $('.slide').length - 1) { //最後
-    $('.next').hide(); //▶️ボタン消す
+    $('.next').hide(); // ▶️ボタン消す
   }
   // 番号の色
   $('.hide').css('background', '#e0f5ff');
@@ -120,17 +83,17 @@ function toggleChange() {
 
 // １、クリックした番号をアクティブにする
 $('.hide').click(function() {
-  $('.active').removeClass('active'); //active状態を全て削除
-  var Index = $('.hide').index($(this)); //clickした番号の
-  $('.slide').eq(Index).addClass('active'); //画像をactiveにする。
+  $('.active').removeClass('active'); // active状態を全て削除
+  var Index = $('.hide').index($(this)); // clickした番号の
+  $('.slide').eq(Index).addClass('active'); // 画像をactiveにする。
   // ◀️▶️ボタン表示
   toggleChange();
 });
 //２、◀️▶とアクティブクラス
 $('.change').click(function() {
-  var $activer = $('.active'); //active状態を
-  $activer.removeClass('active'); //全て削除
-  if ($(this).hasClass('next')) { //▶️ボタンを押した時、
+  var $activer = $('.active'); // active状態を
+  $activer.removeClass('active'); // 全て削除
+  if ($(this).hasClass('next')) { // ▶️ボタンを押した時、
     $activer.next().addClass('active'); // その次をactiveにする
   } else {
     $activer.prev().addClass('active'); // その前をactiveにする
@@ -140,24 +103,27 @@ $('.change').click(function() {
 });
 
 
-// コンタクトフォーム
-$('#age')
-.focus(function() { //クリックした時
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// お問い合わせ
+$('#checked') // JSXでは適用不可？？？
+.focus(function() { // クリックした時
   $(this).css('color', 'green');
 })
-.blur(function() {  //外した時
+.blur(function() {  // 外した時
   $(this).css('color', 'red');
 })
-.change(function() {  //値を変えると、
-  alert('年齢を入力しました');
+.change(function() {  // 値を変えると、
+  alert('確認完了');
 });
 
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// フッター
 // SNSアイコン
-// $('.animated-hover').hover( //マウスが
+// $('.animated-hover').hover( // マウスが
 //   function() {
-//   $(this).animate({'font-size': '18px'}, 300); //のっている時
+//   $(this).animate({'font-size': '18px'}, 300); // のっている時
 //   }, function() {
-//   $(this).animate({'font-size': '16px'}, 300); //のっていない
+//   $(this).animate({'font-size': '16px'}, 300); // のっていない
 //   }
 // );
 
