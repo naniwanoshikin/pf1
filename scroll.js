fbad4944cb2f885143a9ab02a927081d4fc03ec1
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-12 06:52:44 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
 'use strict'; {
   // React.jsの中身には対応していない？
 
-  // chart画像
+  // chart.js
   let node = {
     duration: 900,
     distance: '40%',
@@ -28,15 +28,15 @@
     // viewFactor: 0,
     interval: 400,
   };
-  // タイトル、React類
+  // React.js
   let slideUp = {
     duration: 900, // アニメーションの長さ
-    distance: '50%', // 移動距離
+    distance: '40%', // 移動距離
     origin: 'left', // 下から
     opacity: 0.7, // 薄さ
-    scale: 1.5, // 初期サイズ
+    scale: 2, // 初期サイズ
     delay: 20, // アニメーションの遅延
-    viewFactor: 0.9, // どれくらい見えたら実行するか 0-1
+    viewFactor: 0.8, // どれくらい見えたら実行するか 0-1
     rotate: { // 回転
       x: 50,
       y: 90,
@@ -45,11 +45,11 @@
   };
 
   // 対象
-  let node1 = document.querySelector('.chart');
-  let nodeList1 = document.querySelectorAll('.pic > img');
-  let nodeArray1 = [ // ここにAll入れることはできない？
-    document.querySelector('#wellcome'),
-    document.querySelector('#root'),
+  let node1 = document.querySelector('.chart'); // グラフ
+  let nodeList1 = document.querySelectorAll('.pic > img'); // 写真
+  let nodeArray1 = [ // ここにAll入れることはできない？ // React.js
+    document.querySelector('#root1'),
+    document.querySelector('#root2'),
     document.querySelector('#conta'),
   ];
 
