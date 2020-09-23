af3c6ae1ccd075ec617f31fbd953f218cc6e5e00
Masahiro <67915047+naniwanoshikin@users.noreply.github.com>
2020-09-23 15:35:13 +0900

    Update english.js

--- english.js
+++ english.js
@@ -8,12 +8,12 @@ document.querySelector('.english').addEventListener('click', () => {
       this.id = id;
       this.word = word;
     }
-    // 英訳メソッド
-    convert() { // text属性：書き換え
+    // メソッド
+    convert() { // text属性：書き換え（英訳）
       document.getElementById(this.id).textContent = this.word;
     }
     titl() { // title属性
-      document.getElementById(this.id).title = this.id + 'です！';
+      document.getElementById(this.id).title = 'id名: ' + this.id + 'です！';
       this.convert() // カプセル化
     }
     style() { // style属性
@@ -32,31 +32,34 @@ document.querySelector('.english').addEventListener('click', () => {
       document.getElementById(this.id).textContent = nameNode.dataset.translation;
     }
     static show() { // 静的メソッド
-      console.log('クラスから直接呼び出し');
+      console.log('Englishボタンを押しました');
     }
   }
 
   // インスタンス（オブジェクト）
-  const convers = [ // id, 英単語
-    new Convers('install', 'SHIKITTI WORLD'), // 0 header h1
+  const convers = [ // id, 英訳
+    new Convers('shikitti', 'SHIKITTI WORLD'), // 0 header h1
     new Convers('nav1', 'ABOUT'), // 1 nav 
     new Convers('nav2', 'WORK'), // 2
     new Convers('nav3', 'TOWN'), // 3
     new Convers('nav4', 'CONTACT'), // 4
     new Convers('wellcome', 'Welcome to my Portfolio!'), // 5 見出し
-    new Convers('vani', 'English World!'), // 6 jQ（エラーあり）
+    // new Convers('vani', 'English World!'), // 6 jQ（エラーあり）
     new Convers('about', 'ABOUT'), // 7 自己紹介
-    new Convers('profile', 'PROFILE'), // 8 プロフィール
-    new Convers('open', 'Details'), // 9 年表
-    new Convers('hobby', 'HOBBY'), // 10 趣味
-    new Convers('skill', 'SKILL'), // 11 スキル
-    new Convers('feat', 'WORK'), // 12 特徴
-    new Convers('cafe', 'cafe'), // 13
-    new Convers('member', 'chat'), // 14
-    new Convers('quiz', 'quiz'), // 15
-    new Convers('town', 'THE TOWN'), // 16 地域紹介
-    new Convers('contact', 'CONTACT'), // 17 お問い合わせ
-    new Convers('free', 'Please feel free to contact us'), // 18 p
+    new Convers('myself', 'Let me introduce myself.'), // 
+    new Convers('profile', 'PROFILE'), // 9 プロフィール
+    new Convers('open', 'Details'), // 10 年表
+    new Convers('hobby', 'HOBBY'), // 11 趣味
+    new Convers('skill', 'SKILL'), // 12 スキル
+    new Convers('feat', 'WORK'), // 13 特徴
+    new Convers('see1', 'Please see below.'), // 14
+    new Convers('cafe', 'cafe'), // 15
+    new Convers('chat', 'chat'), // 16
+    new Convers('quiz', 'quiz'), // 17
+    new Convers('town', 'THE TOWN'), // 18 地域紹介
+    new Convers('see2', 'Please click below.'), // 19
+    new Convers('contact', 'CONTACT'), // 20 お問い合わせ
+    new Convers('free', 'Please feel free to contact us!'), // 21 p
   ];
 
   // 実行
@@ -65,9 +68,9 @@ document.querySelector('.english').addEventListener('click', () => {
   }
   convers[0].style(); // h1の色
   // convers[7].clsN();
-  const lists = [5, 7, 12, 16, 17];
+  const lists = [5, 6, 12, 17, 19];
   lists.forEach((e) => {
-    convers[e].togl(); // 各トグルで色変える
+    convers[e].togl(); // クラスをトグル
   });
   Convers.show(); // 静的メソッド
 
